9c1945e35d4946192abcdac8096cf8bb763d9864
Mark Li <53534357+limarkxx@users.noreply.github.com>
2024-05-03 18:21:25 -0500

    add log

--- .vscode/extensions/test-web-extension/src/web/extension.ts
+++ .vscode/extensions/test-web-extension/src/web/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+console.log("test1234");
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
